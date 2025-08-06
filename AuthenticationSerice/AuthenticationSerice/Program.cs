929a60314d87056685529b7aaa9703c3517b50aa
Youfang Yao <yyfsingler@163.com>
2025-08-06 13:30:28 -0700

    finished authorization based on use roles for Weather page

--- AuthenticationSerice/AuthenticationSerice/Program.cs
+++ AuthenticationSerice/AuthenticationSerice/Program.cs
@@ -29,9 +29,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()   // Enables role-based authorization and adds role-related services
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
-    .AddDefaultTokenProviders();
+    .AddDefaultTokenProviders()
+    .AddRoleManager<RoleManager<IdentityRole>>();   // Adds the role manager service for managing roles.
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
