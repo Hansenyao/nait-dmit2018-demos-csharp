4cbc8d7f627d5900406bdf5ad9c542310e073951
Youfang Yao <yyfsingler@163.com>
2025-08-06 12:18:44 -0700

    Add All done project

---
+++ ExampleMudBlazorSolution-Aug05AllDoneIcludingSecurity/ExampleMudSystem/ViewModels/LookupView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleMudSystem.ViewModels
+{
+	public class LookupView
+	{
+		public int LookupID { get; set; }
+
+		public int CategoryID { get; set; }
+		public string Name { get; set; }
+		public bool RemoveFromViewFlag { get; set; }
+	}
+}
